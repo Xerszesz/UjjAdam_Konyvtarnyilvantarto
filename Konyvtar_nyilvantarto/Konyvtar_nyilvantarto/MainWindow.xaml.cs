4933b28d236e69fa02df97a46acdaee7ec4ffead
Ujj Ádám <71527211+Xerszesz@users.noreply.github.com>
2021-03-15 18:49:48 +0100

    Adatok property, és splittelés
    Konyveket mivel el kell választani ezért a annak megcsinálása és propertyket állítani az adatoknak, valamint nemtudom miért nem ment fel az előző comittel de a fájlok beolvasása is ott volt már az előző commit óta

--- Konyvtar_nyilvantarto/Konyvtar_nyilvantarto/MainWindow.xaml.cs
+++ Konyvtar_nyilvantarto/Konyvtar_nyilvantarto/MainWindow.xaml.cs
@@ -13,35 +13,59 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.IO;
 
 namespace Konyvtar_nyilvantarto
 {
    public class Konyvadatok
     {
-        public int ID;
-        public string szerzo;
-        public string cim;
-        public string ev;
-        public string kiado;
-        public bool kolcson;
+        uint getID;
+        public uint ID { get => getID; }
+        public string getszerzo;
+        public string szerzo { get => getszerzo; }
+        public string getcim;
+        public string cim { get => getcim; }
+        public string getev;
+        public string ev { get => getev; }
+        public string getkiado;
+        public string kiado { get => getkiado; }
+        public bool getkolcson;
+        public bool kolcson { get => getkolcson; }
 
+        public Konyvadatok(string sor)
+        {
+            string[] sorElemek = sor.Split(';');
+
+            getID = Convert.ToUInt32(sorElemek[0]);
+            getszerzo = sorElemek[1];
+            getcim = sorElemek[2];
+            getev = sorElemek[3];
+            getkiado = sorElemek[4];
+            getkolcson = Convert.ToBoolean(sorElemek[5]);
+
+        }
     }
 
     public class Tagadatok
     {
-        public int ID;
+        public uint ID;
         public string nev;
         public string lakcim;
     }
 
     public class Kolcsonadatok
     {
-        public int tagID;
-        public int konyvID;
-        public string Datum;
+        public uint tagID;
+        public uint konyvID;
+        public DateTime Datum;
     }
     public partial class MainWindow : Window
     {
+        public string[] fajlhely = new string[3];
+
+        public List<Konyvadatok> Konyvek = new List<Konyvadatok>();
+        public List<Tagadatok> Tagok = new List<Tagadatok>();
+        public List<Kolcsonadatok> Kolcsonadat = new List<Kolcsonadatok>();
         public MainWindow()
         {
             InitializeComponent();
@@ -51,6 +75,11 @@ namespace Konyvtar_nyilvantarto
                 Filter = "txt files (*.txt|*txt)",
                 RestoreDirectory=true
             };
+
+            beolvaso.Title = "Könyvtári adatok beolvasása";
+            beolvaso.ShowDialog();
+            fajlhely[0] = beolvaso.FileName;
+
 
         }
     }
